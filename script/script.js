b797726be26e2cda09f29e575ecc7b639ee469fd
unknown <pedrogfyanez@gmail.com>
2021-08-11 15:59:08 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,64 @@
+const btnMenu = document.querySelectorAll('#btn');
+
+gsap.from(btnMenu, {
+  y: 500,
+  opacity: 0,
+  stagger: 0.05,
+  duration: 0.5,
+  ease: 'back',
+});
+
+btnMenu.forEach(function (btn) {
+  btn.addEventListener('mouseenter', function (e) {
+    const mouseIn = gsap.to(e.target, {
+      duration: 0.2,
+      scale: 1.2,
+      ease: 'back',
+    });
+
+    mouseIn.play();
+  });
+
+  btn.addEventListener('mouseover', function (e) {
+    console.log('mouse is over');
+    const over = gsap.to(e.target, {
+      duration: 0.3,
+      rotate: 10,
+      ease: 'back',
+      onComplete: function () {
+        gsap.to(e.target, {
+          duration: 0.3,
+          rotate: -10,
+          ease: 'back',
+          onComplete: () =>
+            gsap.to(e.target, {
+              repeat: 5,
+            }),
+        });
+      },
+    });
+  });
+
+  btn.addEventListener('mouseleave', function (e) {
+    const mouseOut = gsap.to(e.target, {
+      duration: 0.2,
+      scale: 1,
+      rotate: 0,
+      ease: 'back',
+    });
+    mouseOut.play();
+  });
+});
+
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  scrollHorizontally: true,
+
+  onLeave: function (video, destination, direction) {
+    console.log('Leaving section' + video);
+  },
+});
+
+// var tl = gsap.timeline({ defaults: { duration: 3 } });
+
+//no more repitition of duration: 1!
